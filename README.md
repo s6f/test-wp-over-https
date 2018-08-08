414f496e50f39552410ff87f1e4ab554c116b3a7
s_fukuda <s_fukuda@tam-tam.co.jp>
2018-08-08 10:29:39 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+[wordpress \| Docker Documentation](https://docs.docker.com/samples/library/wordpress/#-via-docker-stack-deploy-or-docker-compose)
+
+# Requirements
+
+- Docker
+  - docker-compose
+
+## Links
+
+- [Docker \- Build, Ship, and Run Any App, Anywhere](https://www.docker.com/)
+- [Install Docker Compose \| Docker Documentation](https://docs.docker.com/compose/install/#prerequisites)
+
---
+++ wp-init.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# as you can see I combined the SSL stuff from @AlexanderOMara 
+# with enabling my own custom vhosts.conf 
+# so I can edit that outside the container
+apt-get update
+apt-get install -y  --no-install-recommends ssl-cert
+rm -r /var/lib/apt/lists/*
+
+a2enmod ssl
+a2dissite 000-default.conf
+a2ensite apache2-vhosts.conf
+
+# finally execute default command
+docker-entrypoint.sh apache2-foreground
+
